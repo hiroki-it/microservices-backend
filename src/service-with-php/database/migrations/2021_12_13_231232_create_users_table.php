3297d474dab9a8bcffe0836d8749054014ca5508
hiroki-it <hasegawafeedshop@gmail.com>
2021-12-14 08:57:54 +0900

    add マイグレーションでusersテーブルを作成できるようにした．

---
+++ src/service-with-php/database/migrations/2021_12_13_231232_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements("id")
+                ->comment("ユーザID");
+            $table->string("name")
+                ->comment("ユーザ名");
+            $table->string('email_address')
+                ->unique()
+                ->comment('メールアドレス');
+
+            $table->systemColumns();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
