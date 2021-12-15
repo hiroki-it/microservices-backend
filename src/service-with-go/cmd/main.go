38728a5cc48233730d9524bc6281bdc0f6d4fdf0
hiroki-it <hasegawafeedshop@gmail.com>
2021-12-15 20:59:49 +0900

    update POSTのエンドポイントを追加した．

--- src/service-with-go/cmd/main.go
+++ src/service-with-go/cmd/main.go
@@ -4,12 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Input struct {
+	Number int `json:"number"`
+}
+
 func main() {
 	r := gin.Default()
 
 	r.GET("/swg/", func(c *gin.Context) {
 		c.String(200, "Hello world!")
 	})
+
+		r.POST("/", func(c *gin.Context) {
+			input := &Input{}
+			_ = c.Bind(input)
+			result := input.Number * 2
+			c.JSON(200, result)
+		})
+	}
 
 	r.Run()
 }
