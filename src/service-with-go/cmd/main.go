b77b58f672beac9e718712aa77c573bc3730a70e
hiroki-it <hasegawafeedshop@gmail.com>
2021-12-15 20:29:29 +0900

    update コードを整形した．

--- src/service-with-go/cmd/main.go
+++ src/service-with-go/cmd/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
-    
-    r.GET("/swg/", func(c *gin.Context) {
-        c.String(200, "Hello world!")
-    })
-    
-    r.Run()
+	r := gin.Default()
+
+	r.GET("/swg/", func(c *gin.Context) {
+		c.String(200, "Hello world!")
+	})
+
+	r.Run()
 }
