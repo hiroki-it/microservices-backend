d8e4b9e5e76bd339360932d751f2a5ce9f9c292c
hiroki-it <hasegawafeedshop@gmail.com>
2021-12-19 21:37:44 +0900

    add マイグレーションで，ordersテーブルを作成できるようにした．

---
+++ src/order-service/database/migrations/2021_12_19_121739_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements("id")
+                ->comment("受注ID");
+            $table->bigIncrements("food_id")
+                ->comment("食べ物ID");
+            $table->bigIncrements("drink_id")
+                ->comment("飲み物ID");
+
+            $table->systemColumns();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
