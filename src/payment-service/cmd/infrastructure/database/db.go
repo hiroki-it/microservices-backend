faff7dff8d8e1d1a2b832d083fa0d476d21fd8a4
hiroki-it <hasegawafeedshop@gmail.com>
2021-12-22 21:32:56 +0900

    fix パラメータの順番を修正した．

--- src/payment-service/cmd/infrastructure/database/db.go
+++ src/payment-service/cmd/infrastructure/database/db.go
@@ -12,8 +12,8 @@ import (
 func NewDB() (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_DATABASE"),
