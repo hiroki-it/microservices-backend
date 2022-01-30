7bf6fb08d3186647831baa9ddcb9d7f21182d261
hiroki-it <hasegawafeedshop@gmail.com>
2022-01-30 21:07:05 +0900

    update エントリーポイントでFlaskクラスのインスタンスを作成するようにした．

--- src/customer/src/main.py
+++ src/customer/src/main.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
+from flask import Flask
 from infrastructure.routers.router import run
 
 load_dotenv(override=True)
+
+app = Flask(__name__)
 
 if __name__ == '__main__':
     run(app)
