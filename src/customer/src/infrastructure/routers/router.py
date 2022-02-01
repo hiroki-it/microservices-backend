4f5611925437b5c9b1ba131ac1bd92c71c251461
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-01 21:43:33 +0900

    update 複数形にした．

--- src/customer/src/infrastructure/routers/router.py
+++ src/customer/src/infrastructure/routers/router.py
@@ -3,7 +3,7 @@ from src.interface.controllers.customer_controller import CustomerController
 def set_routes(app):
 
     # customer
-    PREFIX_CUSTOMER = "customer"
+    PREFIX_CUSTOMER = "customers"
     @app.route("/{PREFIX_CUSTOMER}/show", methods=["GET"])
     customerController = CustomerController()
     customerController.show_customer()
