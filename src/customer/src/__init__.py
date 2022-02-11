eb867febe99de5d2a35e628e66bf99f34f3455d9
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-12 01:58:41 +0900

    update アプリケーションファクトリーパターンに則って，create_appメソッド内でsrcディレクトリを読み込むようにした．

--- src/customer/src/__init__.py
+++ src/customer/src/__init__.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from flask import Flask
-from src.infrastructure.routers.router import set_routes
 
 def create_app():
+    from src.infrastructure.routers.router import set_routes
+
     app = Flask(__name__)
     set_routes(app)
     return app
