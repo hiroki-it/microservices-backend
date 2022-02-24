fbe0388dd0d2740d345fd14f9ea6b0a3a4c3d4e0
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-24 20:13:18 +0900

    update 不要な実装を削除した．

--- src/customer/src/controllers/controller.py
+++ src/customer/src/controllers/controller.py
@@ -1,9 +1,2 @@
-from fastapi import Depends
-from src.database.db import DB
-
 class Controller():
-
-    # コンストラクタ
-    def __init__(self):
-        db = DB()
-        self.db = db.get_db()
+    pass
