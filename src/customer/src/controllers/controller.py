bf908143fcfabe4d23b885e20f9fb53a312a5c49
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 17:23:24 +0900

    update コードを整形した．

--- src/customer/src/controllers/controller.py
+++ src/customer/src/controllers/controller.py
@@ -2,9 +2,10 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from src.database.db import get_db
 
-class Controller():
 
-     # コンストラクタ
-     # @see https://fastapi.tiangolo.com/tutorial/dependencies/
-     def __init__(self, db: Session = Depends(get_db)):
-         self.db = db
+class Controller:
+
+    # コンストラクタ
+    # @see https://fastapi.tiangolo.com/tutorial/dependencies/
+    def __init__(self, db: Session = Depends(get_db)):
+        self.db = db
