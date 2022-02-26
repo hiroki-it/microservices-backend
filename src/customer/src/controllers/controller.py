3e27b2d3ee60fedc0615d5b155c46cf3a691c9ce
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 22:46:21 +0900

    update 全てのコントローラーで使用できるJSONレスポンス用のメソッドを定義した．

--- src/customer/src/controllers/controller.py
+++ src/customer/src/controllers/controller.py
@@ -1,6 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from src.database.db import get_db
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 
 class Controller:
@@ -19,3 +21,7 @@ class Controller:
         self.db.refresh(model)
 
         return model
+
+    # モデルをJSONレスポンスで返却します．
+    def send_json_response(self, model):
+        return JSONResponse(jsonable_encoder(model))
