b478f0d8a079b296de079f47768003d2e73b38e3
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 17:18:08 +0900

    update コードを整形した．

--- src/customer/src/database/db.py
+++ src/customer/src/database/db.py
@@ -5,21 +5,19 @@ from sqlalchemy.orm import sessionmaker
 # SessionLocalクラスを作成します．
 # @see https://fastapi.tiangolo.com/ja/tutorial/sql-databases/#create-a-sessionlocal-class
 def create_session_local(self):
-        db_url = "{db_driver}://{db_user}:{db_password}@{db_host}/{db_database}?charset=utf8".format(
-                db_driver = os.getenv("DB_DRIVER"),
-                db_user = os.getenv("DB_USER"),
-                db_password = os.getenv("DB_PASSWORD"),
-                db_host = os.getenv("DB_HOST"),
-                db_database = os.getenv("DB_DATABASE")
-            )
+    db_url = "{db_driver}://{db_user}:{db_password}@{db_host}/{db_database}?charset=utf8".format(
+        db_driver=os.getenv("DB_DRIVER"),
+        db_user=os.getenv("DB_USER"),
+        db_password=os.getenv("DB_PASSWORD"),
+        db_host=os.getenv("DB_HOST"),
+        db_database=os.getenv("DB_DATABASE"),
+    )
 
-        engine = create_engine(
-            db_url,
-            connect_args = {"check_same_thread": False}
-        )
-        
-        return sessionmaker(autocommit=False, autoflush=False, bind=engine)
- 
+    engine = create_engine(db_url, connect_args={"check_same_thread": False})
+
+    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 # DBに接続します．
 # @see https://fastapi.tiangolo.com/ja/tutorial/sql-databases/#create-a-dependency
 def get_db(self):
@@ -28,4 +26,3 @@ def get_db(self):
         yield session_local
     finally:
         session_local.close()
-
