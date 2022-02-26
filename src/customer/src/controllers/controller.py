aa11628f7a1bd8e53fa01821df22a4ee03d65abd
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 22:41:00 +0900

    update 全てのコントローラーで使用できる作成トランザクション用のメソッドを定義した．

--- src/customer/src/controllers/controller.py
+++ src/customer/src/controllers/controller.py
@@ -8,3 +8,14 @@ class Controller:
     # コンストラクタ
     def __init__(self, db: Session):
         self.db = db
+
+    # 作成トランザクションを実行します．
+    # @see https://fastapi.tiangolo.com/ja/tutorial/sql-databases/#create-data
+    def create(self, model):
+        self.db.add(model)
+
+        self.db.commit()
+
+        self.db.refresh(model)
+
+        return model
