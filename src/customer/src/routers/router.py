eb94918554be7df8de9b7a7b0d25e38b01352894
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 21:19:37 +0900

    update routerオブジェクトを使用するようにした．

--- src/customer/src/routers/router.py
+++ src/customer/src/routers/router.py
@@ -1,3 +1,7 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from src.database.db import get_db
+
 def set_routes(app):
     from src.controllers.customer_controller import CustomerController
 
@@ -5,10 +9,10 @@ def set_routes(app):
     PREFIX_CUSTOMER = "customers"
     customerController = CustomerController()
 
-    @app.route("/{PREFIX_CUSTOMER}/", methods=["GET"])
+    @router.get("/{PREFIX_CUSTOMER}/")
     def route_index_customer():
         return customerController.index_customer()
 
-    @app.route("/{PREFIX_CUSTOMER}/", methods=["POST"])
+    @router.post("/{PREFIX_CUSTOMER}/")
     def route_create_customer():
         return customerController.create_customer()
