c9d89f466480fad0db15e0fbad300e45c20e27b0
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 21:04:08 +0900

    update 不要な引数を削除した．

--- src/customer/src/routers/router.py
+++ src/customer/src/routers/router.py
@@ -6,9 +6,9 @@ def set_routes(app):
     customerController = CustomerController()
 
     @app.route("/{PREFIX_CUSTOMER}/", methods=["GET"])
-    def route_index_customer(self):
+    def route_index_customer():
         return customerController.index_customer()
 
     @app.route("/{PREFIX_CUSTOMER}/", methods=["POST"])
-    def route_create_customer(self):
+    def route_create_customer():
         return customerController.create_customer()
