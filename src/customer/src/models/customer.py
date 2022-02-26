b69098a1b36818df4b4ae0204528511316df2c95
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-26 17:26:06 +0900

    update テーブル名を変更した．

--- src/customer/src/models/customer.py
+++ src/customer/src/models/customer.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 
 
 class Customer(Base):
-    __tablename__ = "Customers"
+    __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
