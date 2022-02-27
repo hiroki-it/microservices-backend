81454d8e5101e509fda7ea525c2ff4e2a82fbdf0
hiroki-it <hasegawafeedshop@gmail.com>
2022-02-27 22:35:09 +0900

    update エンドポイントからレスポンスを返信できるようにした．

--- src/orchestrator/src/routers/router.py
+++ src/orchestrator/src/routers/router.py
@@ -1,8 +1,13 @@
 # @see https://fastapi.tiangolo.com/tutorial/bigger-applications/#apirouter
 # @see https://fastapi.tiangolo.com/tutorial/dependencies/
 def create_router():
-    from fastapi import APIRouter, Depends
+    from fastapi import APIRouter
+    from fastapi.responses import JSONResponse
 
     router = APIRouter()
+    
+    @router.get("/checkout/")
+    async def checkout():
+         return JSONResponse({"Hello" : "World"})
 
     return router
