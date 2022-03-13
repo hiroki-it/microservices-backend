3ccbae588081aa397fedfbd5c6f91140e20da5c6
hiroki-it <hasegawafeedshop@gmail.com>
2022-03-13 18:32:55 +0900

    fix アカウントIDを出力するようにした．

--- ops/export_envs.sh
+++ ops/export_envs.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 # 環境変数を定義します．
-echo 'export AWS_ECR_ACCOUNT_URL="${AWS_ACCESS_KEY_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"' >> $BASH_ENV
+echo 'export AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"' >> $BASH_ENV
 
 # 環境変数を出力します．
 source $BASH_ENV
